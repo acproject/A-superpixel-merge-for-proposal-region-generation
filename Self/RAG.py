c0259299d357f322b10f29c99c412ac3684bcec8
qinhangyu <acproject@qq.com>
2020-06-03 16:50:55 +0800

    add imgs

--- Self/RAG.py
+++ Self/RAG.py
@@ -3,10 +3,11 @@ from skimage.future import graph
 from skimage.measure import regionprops
 import numpy as np
 import cv2;
-from  PIL import Image;
+from PIL import Image;
 from matplotlib import pyplot as plt, colors
+import openslide
 
-
+# slide = openslide.OpenSlide('/home/qinhangyu/data/11/乳腺/2019-11-19 16.21.54.ndpi');
 def _weight_mean_color(graph, src, dst, n):
     ''':cvar
     Callback to handle merging nodes by recomputing mean color.
@@ -46,7 +47,7 @@ def merge_mean_color(graph, src ,dst):
     graph.nodes[dst]['pixel count'] += graph.nodes[src]['pixel count']
     graph.nodes[dst]['mean color'] = (graph.nodes[dst]['total color'] / graph.nodes[dst]['pixel count'])
 
-img = Image.open('../images/12.png')
+img = Image.open('../images/16.png')
 img = img.convert("RGB")
 
 img = np.array(img)
@@ -55,17 +56,23 @@ img = np.array(img)
 # print("img:", type(img))
 
 
-labels = segmentation.slic(img, compactness=10, n_segments=1200)
+labels = segmentation.slic(img, compactness=10, n_segments=80000)
 g = graph.rag_mean_color(img, labels, mode='distance')
-cmap = colors.ListedColormap(['#6599FF', '#ff9900'])
+# cmap = colors.ListedColormap(['#6599FF', '#ff9900'])
 # graph.show_rag(labels, g, img, img_cmap=cmap)
 
-labels2 = graph.merge_hierarchical(labels, g, thresh=30, rag_copy=False,
+labels2 = graph.merge_hierarchical(labels, g, thresh=45, rag_copy=False,
                                    in_place_merge=True, merge_func=merge_mean_color,
                                    weight_func=_weight_mean_color)
-out = color.label2rgb(labels2, img, kind='overlay', bg_label=255)
-# out2 = color.label2rgb(labels2, img, kind='overlay', bg_label=2)
-out = segmentation.mark_boundaries(out, labels2, (0, 0, 0))
+# out = color.label2rgb(labels2, img, kind='avg', bg_label=255)
+out2 = color.label2rgb(labels2, img, kind='avg')
+out2 = segmentation.mark_boundaries(out2, labels2, (0,0,0))
 
-io.imshow(out)
-io.show()
+# # io.imshow(out2)
+# io.imshow(out2)
+# io.show()
+plt.figure(figsize=(12,8))
+plt.title("USE AVG")
+plt.imshow(out2)
+plt.show()
+
